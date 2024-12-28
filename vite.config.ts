bbeac2fdb97a276f1e32a7b8ed970e888def6fb5
Adityagnss <adityagnss@gmail.com>
2024-12-28 23:42:37 +0530

    com1

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/website-for-acmw/", // YOUR REPO NAME HERE
+  base: "/", // Changed for local development
 });
