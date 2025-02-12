55d6bd1d610c82719f70b605666f62d62dc3b29f
GADEY ADITYA <113210100+Adityagnss@users.noreply.github.com>
2025-02-12 22:41:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/website-for-acmw/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  }
+  base: '/',
 });
